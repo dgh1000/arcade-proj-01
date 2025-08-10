c80a75ff6aa261d46082ea45875e9b63ae735e35
Mike Mossey <michaelmossey@gmail.com>
2025-08-09 19:43:59 -0700

    updated to Zayn's version

--- spaceship_me.py
+++ spaceship_me.py
@@ -12,41 +12,28 @@ WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 WINDOW_TITLE = "Spaceship Example"
 
-class Asteroid(arcade.Sprite):
+# it IS a sprite. It's both a Sprite and a bullet
+# classes: silverware, class of all knives, forks and spoons
+# subclass: stainless steel silverware
+# subclass: plastic silverware
+# instance of subclass plastic. is it also an instance of silverware. yes!
+# if something is instance of silverware, is it also an instance of plastic? maybe
+# 
 
-    def __init__(self, init_pos, angle, vel):
-        img = "asteroid.png"
+class Movable(arcade.Sprite):
+    def __init__(self, img, init_pos, vel_x, vel_y):
         super().__init__(img, 0.2)
-        self.position = init_pos[0], init_pos[1]
-        self.angle_radians = (angle - 90) * math.pi / 180
-        self.vel_x = vel * math.cos(self.angle_radians)
-        self.vel_y = -vel * math.sin(self.angle_radians)
+        self.position = init_pos
+        self.vel_x = vel_x 
+        self.vel_y = vel_y
 
-    def update(self, dtime):
+    def update(self, delta_time):
+        # update the sprite's position. self.vel_x, self.vel_y
         x, y = self.position
-        self.position = x+self.vel_x, y+self.vel_y
-        if self.position[0] < 0 or self.position[0] > WINDOW_WIDTH:
+        if x < 0 or x > WINDOW_WIDTH or y < 0 or y > WINDOW_HEIGHT:
             self.kill()
-        if self.position[1] < 0 or self.position[1] > WINDOW_HEIGHT:
-            self.kill()
-
-class Bullet(arcade.Sprite):
-
-    def __init__(self, init_pos, angle, vel):
-        super().__init__("bullet.png", 0.2)
-        self.position = init_pos[0], init_pos[1]
-        self.angle_radians = (angle - 90) * math.pi / 180
-        self.vel_x = vel * math.cos(self.angle_radians)
-        self.vel_y = -vel * math.sin(self.angle_radians)
-
-    def update(self, dtime):
-        x, y = self.position
-        self.position = x+self.vel_x, y+self.vel_y
-        if self.position[0] < 0 or self.position[0] > WINDOW_WIDTH:
-            self.kill()
-        if self.position[1] < 0 or self.position[1] > WINDOW_HEIGHT:
-            self.kill()
-
+            return
+        self.position = (self.vel_x+x, self.vel_y+y)
 class GameView(arcade.View):
 
     def __init__(self):
@@ -57,99 +44,99 @@ class GameView(arcade.View):
         # Variables that will hold sprite lists
         self.spaceship_list = None
         self.bullet_list = None
-        self.asteroid_list = None
 
         # Create a variable to hold the player sprite
         self.spaceship_sprite = None
+        self.bullet_sprite = None
 
         # Hide the mouse cursor while it's over the window
         self.window.set_mouse_visible(False)
-
         self.background_color = arcade.color.BLACK
-        
         self.keys_down = set()
-
         self.angle = 0
+        self.count = 0
 
     def setup(self):
         """ Set up the game and initialize the variables. """
 
         # Create the sprite lists
 
+        # Set up the spaceship
         self.spaceship_sprite = arcade.Sprite("spaceship.png", scale=0.1)
         self.spaceship_sprite.position = WINDOW_WIDTH//2, WINDOW_HEIGHT//2
         self.spaceship_list = arcade.SpriteList()
         self.spaceship_list.append(self.spaceship_sprite)
-        self.asteroid_list = arcade.SpriteList()
-        self.asteroid_list.append(Asteroid((300,300), 0, 1))
 
         # Set up the bullet
-        # self.bullet_sprite = Bullet((300, 300), 0, 2)
+        # self.bullet_sprite = Bullet((WINDOW_WIDTH//2, WINDOW_HEIGHT//2), 0, 1)  # arcade.Sprite("bullet.png", scale=0.2)
         self.bullet_list = arcade.SpriteList()
         # self.bullet_list.append(self.bullet_sprite)
 
-
-    def on_draw(self):
-        """ Draw everything """
-
-        # Clear the screen to only show the background color
-        self.clear()
-
-        # Draw the sprites
-        self.spaceship_list.draw()
-        self.bullet_list.draw()
-        self.asteroid_list.draw()
-
-    def handle_collisions(self):
-        for b in self.bullet_list:
-            cols = arcade.check_for_collision_with_list(b, self.asteroid_list)
-            for a in cols:
-                a.remove_from_sprite_lists()
+        # asteroid list
+        self.asteroid_list = arcade.SpriteList()
+        
+    def new_bullet(self):
+        angle_radians = (self.spaceship_sprite.angle-90)*math.pi/180
+        vel_x = 1 * math.cos(angle_radians)
+        vel_y = -1 * math.sin(angle_radians)
+        x, y = self.spaceship_sprite.position
+        self.bullet_list.append(Movable("bullet.png", (x, y), vel_x, vel_y))
 
     def new_asteroid(self):
-        i = random.randint(0, 3)
-        n = None
-        if i == 0:
-            # add along bottom border
-            n = Vector(random.randint(0, WINDOW_WIDTH), 0)
-        elif i == 1:
-            # along top border
-            n = Vector(random.randint(0, WINDOW_WIDTH), WINDOW_HEIGHT)
-        elif i == 2:
-            # add along left border
-            n = Vector(0, random.randint(0, WINDOW_HEIGHT))
-        else:
-            # add along right border
-            n = Vector(WINDOW_WIDTH, random.randint(0, WINDOW_HEIGHT))
-        center = Vector(WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
-        d = center - n
-        a = d.angle() * 180 / math.pi
-        self.asteroid_list.append(Asteroid((n.x, n.y), a, 1))
-
-    def update_spaceship():
-        pass
-
-    def on_update(self, delta_time):
-        self.handle_collisions()
-
-        """ Movement and game logic """
-        self.bullet_list.update()
+        left = (0, random.randint(0, WINDOW_HEIGHT))
+        right = (WINDOW_WIDTH, random.randint(0, WINDOW_HEIGHT))
+        top = (random.randint(0, WINDOW_WIDTH), WINDOW_HEIGHT)
+        bottom = (random.randint(0, WINDOW_WIDTH), 0)
+        # pos1, pos2 = random.choice([left, right, top, bottom])
+        p = Vector(*random.choice([left, right, top, bottom]))
+        x, y = self.spaceship_sprite.position
+        ss = Vector(x, y)
+        point = (ss - p).normalize()
+        vel_x = point.x
+        vel_y = point.y
+        self.asteroid_list.append(Movable("asteroid.png", (p.x, p.y), vel_x, vel_y))
+        
+    def spaceship_update(self):
         if arcade.key.LEFT in self.keys_down:
             self.angle -= 2
         if arcade.key.RIGHT in self.keys_down:
             self.angle += 2
         self.spaceship_sprite.angle = self.angle
-        self.asteroid_list.update()
 
-        if 1 == random.randint(0, 45):
+    def check_collisions(self):
+        need_removal = []
+        for asteroid in self.asteroid_list:
+            collisions = arcade.check_for_collision_with_list(asteroid, self.bullet_list)
+            for bullet in collisions:
+                print(f"Bullet hit asteroid! asteroid at {asteroid.center_x}, {asteroid.center_y}, Bullet at {bullet.center_x}, {bullet.center_y}")
+                need_removal.append(asteroid)
+                need_removal.append(bullet)
+        for remove in need_removal:
+            remove.remove_from_sprite_lists()
+
+    def on_draw(self):
+        """ Draw everything """
+        # Clear the screen to only show the background color
+        self.clear()
+        # Draw the sprites
+        self.spaceship_list.draw()
+        self.bullet_list.draw()
+        self.asteroid_list.draw()
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+        self.count += 1
+        if self.count % 120 == 0:
+            # print("check")
             self.new_asteroid()
+        self.spaceship_update()
+        self.bullet_list.update()
+        self.asteroid_list.update()
+        self.check_collisions()
 
     def on_key_press(self, key, modifier):
-        if key == arcade.key.ESCAPE:
-            arcade.close_window()
-        elif key == arcade.key.SPACE:
-            b = Bullet((WINDOW_WIDTH//2, WINDOW_HEIGHT//2), self.spaceship_sprite.angle, 2)
-            self.bullet_list.append(b)
+        if key == arcade.key.SPACE:
+            self.new_bullet()
         else:
             self.keys_down.add(key)
 
